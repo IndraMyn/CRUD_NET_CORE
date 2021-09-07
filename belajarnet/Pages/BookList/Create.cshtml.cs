20e30e6a96b2dd14da81864fb41536f5ce716c21
Indra Mulyawan <indraawan13@gmail.com>
2021-09-07 11:33:08 +0700

    Add project files.

---
+++ belajarnet/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using belajarnet.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace belajarnet.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [TempData]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+
+            // for validation (required)
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Book.Add(Book);
+            await _db.SaveChangesAsync();
+
+            Message = "Book has been created successfully!";
+
+            return RedirectToPage("Index");
+        }
+        
+    }
+}
---
+++ belajarnet/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using belajarnet.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace belajarnet.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [TempData]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+            // Book = _db.Book.Where(b => b.Id == id).FirstOrDefault();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+
+                Message = "Book has been updated successfully!";
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+        }
+    }
+}
